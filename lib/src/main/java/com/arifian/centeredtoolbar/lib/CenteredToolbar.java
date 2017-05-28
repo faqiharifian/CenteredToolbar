cc32d3d02000a9853fc6c179cf3e3678187d5933
Faqih Arifian <faqiharifianaji@gmail.com>
2017-05-28 13:35:15 +0700

    add attrs and modify java to add more customization

--- lib/src/main/java/com/arifian/centeredtoolbar/lib/CenteredToolbar.java
+++ lib/src/main/java/com/arifian/centeredtoolbar/lib/CenteredToolbar.java
@@ -1,10 +1,12 @@
 package com.arifian.centeredtoolbar.lib;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -21,7 +23,8 @@ public class CenteredToolbar extends Toolbar {
     private final String DO_TYPEFACE = "typeface";
     private final String DO_SIZE = "size";
 
-    private final int INVALID_UNIT = -7423894;
+    private final int INVALID_UNIT = -1;
+
 
     public CenteredToolbar(Context context) {
         this(context, null);
@@ -29,6 +32,18 @@ public class CenteredToolbar extends Toolbar {
 
     public CenteredToolbar(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, android.support.v7.appcompat.R.attr.toolbarStyle);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CenteredToolbar);
+        float titleSize = a.getDimension(R.styleable.CenteredToolbar_titleTextSize, getTitleTextSize());
+        float subtitleSize = a.getDimension(R.styleable.CenteredToolbar_subtitleTextSize, getSubtitleTextSize());
+        String titleTypeface = a.getString(R.styleable.CenteredToolbar_titleTypeface);
+        String subtitleTypeface = a.getString(R.styleable.CenteredToolbar_subtitleTypeface);
+        a.recycle();
+
+        setTitleTextSize(TypedValue.COMPLEX_UNIT_PX, titleSize);
+        setSubtitleTextSize(TypedValue.COMPLEX_UNIT_PX, subtitleSize);
+        setTitleTypeface(titleTypeface);
+        setSubtitleTypeface(subtitleTypeface);
     }
 
     public CenteredToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -83,7 +98,16 @@ public class CenteredToolbar extends Toolbar {
         changeSize(getTextView(SUBTITLE), unit, size);
     }
 
+    public TextView getTitleTextView(){
+        return getTextView(TITLE);
+    }
+
+    public TextView getSubtitleTextView(){
+        return getTextView(SUBTITLE);
+    }
+
     private TextView getTextView(String what){
+        TextView textView = null;
         final CharSequence originalTitle = getTitle();
         final CharSequence originalSubtitle = getSubtitle();
 
@@ -94,12 +118,12 @@ public class CenteredToolbar extends Toolbar {
             View view = getChildAt(i);
 
             if(view instanceof TextView){
-                final TextView textView = (TextView) view;
+                final TextView tempTextView = (TextView) view;
 
-                if(textView.getText().equals(TITLE) && what.equals(TITLE)){
-                    return textView;
-                } else if(textView.getText().equals(SUBTITLE) && what.equals(SUBTITLE)){
-                    return textView;
+                if(tempTextView.getText().equals(TITLE) && what.equals(TITLE)){
+                    textView = tempTextView;
+                } else if(tempTextView.getText().equals(SUBTITLE) && what.equals(SUBTITLE)){
+                    textView = tempTextView;
                 }
             }
         }
@@ -107,7 +131,17 @@ public class CenteredToolbar extends Toolbar {
         super.setTitle(originalTitle);
         super.setSubtitle(originalSubtitle);
 
-        return null;
+        return textView;
+    }
+
+    private float getTitleTextSize(){
+        TextView textView = getTextView(TITLE);
+        return textView.getTextSize();
+    }
+
+    private float getSubtitleTextSize(){
+        TextView textView = getTextView(SUBTITLE);
+        return textView.getTextSize();
     }
 
     private void centerizeTextView(final TextView textView){
@@ -144,8 +178,12 @@ public class CenteredToolbar extends Toolbar {
     }
 
     private void changeTypeface(TextView textView, String path){
-        Typeface font = Typeface.createFromAsset(getContext().getAssets(), path);
-        textView.setTypeface(font);
+        if(path != null) {
+            if (!path.isEmpty()) {
+                Typeface font = Typeface.createFromAsset(getContext().getAssets(), path);
+                textView.setTypeface(font);
+            }
+        }
     }
 
     private void changeSize(TextView textView, int unit, float size){
